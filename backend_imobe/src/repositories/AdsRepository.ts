9cffd323b912a5f345e2b5ef08b9baf72304333d
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-23 23:43:46 -0300

    Created repository ads

---
+++ backend_imobe/src/repositories/AdsRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Ads } from "../entities/Ads";
+
+@EntityRepository(Ads)
+class AdsRepository extends Repository<Ads> {}
+
+export { AdsRepository };
