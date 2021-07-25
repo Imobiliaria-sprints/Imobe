3ef9f3db03e1f7fe865de2e5b81fc72be3010621
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-25 15:15:28 -0300

    Created list ads with user service

--- backend_imobe/src/useCases/ListAdsUserUseCase/ListAdsUserUseCase.ts
+++ backend_imobe/src/useCases/ListAdsUserUseCase/ListAdsUserUseCase.ts
@@ -1,11 +1,9 @@
 import { getCustomRepository } from "typeorm";
 import { Ads } from "../../entities/Ads";
-import { ICreateAdsUseCase } from "../../interfaces/ICreateAdsUseCase";
 import { AdsRepository } from "../../repositories/AdsRepository";
-import { ICreateAdsDTO } from "../CreateAdsUseCase/ICreateAdsDTO";
 
-class ListAdsUserUseCase implements ICreateAdsUseCase {
-  async execute({ user_id }: ICreateAdsDTO): Promise<Ads[]> {
+class ListAdsUserUseCase {
+  async execute(user_id: string): Promise<Ads[]> {
     const adsRepository = getCustomRepository(AdsRepository);
 
     const allAds = await adsRepository.find({
