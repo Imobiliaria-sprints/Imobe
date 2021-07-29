457094c8dfae639023e33eca70be8210a0afdc57
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-29 20:45:01 -0300

    Started home page

--- frontend_imobe/src/components/Header/index.tsx
+++ frontend_imobe/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import { ReactNode } from "react";
 import styles from "./style.module.scss";
@@ -9,7 +10,7 @@ type HeaderProps = {
 export function Header({ children }: HeaderProps) {
   return (
     <div className={styles.headerContainer}>
-      <Image height={60} width={210} src="/icons/logo.svg" alt="Logomarca" />
+      <img src="/icons/logo.svg" alt="Logomarca" />
       <nav>{children}</nav>
     </div>
   );
--- frontend_imobe/src/pages/index.tsx
+++ frontend_imobe/src/pages/index.tsx
@@ -1,13 +1,39 @@
-import { Header } from "./components/Header";
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import { Header } from "../components/Header";
+import styles from "../styles/pages/home.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.home}>
       <Header>
         <a>Entre em contato</a>
         <a>Entre em sua conta</a>
         <button>Faça sua conta</button>
       </Header>
+
+      <section className={styles.section_apresentation}>
+        <div>
+          <h1>Encontre o lugar perfeito para você!</h1>
+          <span>lorem ipsum dolar amet lorem ipsum dolar amet </span>
+          <ul>
+            <li>Agende sua visita ao seu imóvel favorito</li>
+            <li>Divulgue seu imóvel</li>
+            <li>Torne seu sonho realidade</li>
+          </ul>
+          <button>
+            <img src="/icons/house.svg" alt="house icons" />
+            Ver imóveis
+          </button>
+        </div>
+        <div>
+          <img
+            src="/img/houses.png"
+            alt="Apresentation Imobeflex"
+            draggable="false"
+          />
+        </div>
+      </section>
     </div>
   );
 }
