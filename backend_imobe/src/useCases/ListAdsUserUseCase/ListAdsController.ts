3133f8a1710a6f6986c48e089c4be5125694e92a
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-07-31 14:56:45 -0300

    Created simple pagination

--- backend_imobe/src/useCases/ListAdsUserUseCase/ListAdsController.ts
+++ backend_imobe/src/useCases/ListAdsUserUseCase/ListAdsController.ts
@@ -1,13 +1,22 @@
 import { Request, Response } from "express";
 import { ListAdsUserUseCase } from "./ListAdsUserUseCase";
 
+interface QueryRequest {
+  limit?: number;
+  skip?: number;
+}
+
 class ListAdsController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { user_id } = request;
-
+    const { limit, skip }: QueryRequest = request.query;
     const listAdsUserUseCase = new ListAdsUserUseCase();
 
-    const ads = await listAdsUserUseCase.execute(user_id);
+    const ads = await listAdsUserUseCase.execute(
+      user_id,
+      Number(limit),
+      Number(skip)
+    );
 
     return response.json(ads);
   }
--- backend_imobe/src/useCases/ListAdsUserUseCase/ListAdsUserUseCase.ts
+++ backend_imobe/src/useCases/ListAdsUserUseCase/ListAdsUserUseCase.ts
@@ -4,12 +4,19 @@ import { Ads } from "../../entities/Ads";
 import { AdsRepository } from "../../repositories/factory/AdsRepository";
 
 class ListAdsUserUseCase {
-  async execute(user_id: string): Promise<Record<string, any>> {
+  async execute(
+    user_id: string,
+    limit?: number,
+    skip = 0
+  ): Promise<Record<string, any>> {
     const adsRepository = getCustomRepository(AdsRepository);
 
     const allAds = await adsRepository.find({
       where: { user_id },
+      order: { created_at: "DESC" },
       relations: ["userId"],
+      skip: skip,
+      take: limit,
     });
 
     return classToPlain(allAds);
