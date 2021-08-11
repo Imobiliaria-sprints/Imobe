670f29a242692dc794347944a78396405e6b11a6
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-10 21:18:44 -0300

    Created Address routes

--- backend_imobe/src/router.ts
+++ backend_imobe/src/router.ts
@@ -6,6 +6,7 @@ import CreateAdsController from "./useCases/CreateAdsUseCase/CreateAdsController
 import ListAdsController from "./useCases/ListAdsUserUseCase/ListAdsController";
 import { validationUser } from "./useCases/CreateUserUseCase/validationUser";
 import { validationAds } from "./useCases/CreateAdsUseCase/validationAds";
+import CreateAddressController from "./useCases/CreateAddressUseCase/CreateAddressController";
 
 const router = Router();
 
@@ -17,6 +18,7 @@ router.post(
   validationAds,
   CreateAdsController.handle
 );
+router.post("/address/:ads_id", CreateAddressController.handle);
 router.get("/dashboard", ensureAuthenticated, ListAdsController.handle);
 
 export { router };
