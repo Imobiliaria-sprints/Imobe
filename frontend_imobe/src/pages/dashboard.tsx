98473fc9db52f10d3624de1fac086b9346d39415
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-15 20:04:49 -0300

    Get post data from imobe backend

--- frontend_imobe/src/pages/dashboard.tsx
+++ frontend_imobe/src/pages/dashboard.tsx
@@ -1,16 +1,31 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { parseCookies } from "nookies";
-import { useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
-import { api } from "../services/api";
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 import { getApiClient } from "../services/axios";
 
-export default function Dashboard() {
+type DashboardProps = {
+  posts: PostProps[];
+};
+
+type PostProps = {
+  id: string;
+  title: string;
+  rooms: number;
+  price: number;
+  square_meters: string;
+  created_at: string;
+};
+
+export default function Dashboard({
+      posts,
+    }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { user } = useAuth();
 
   return (
     <div>
-      <h1>Hello {user?.name}</h1>
+      <aside></aside>
     </div>
   );
 }
@@ -19,6 +34,21 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const apiClient = getApiClient(ctx);
 
   const { ["imobeflex.token"]: token } = parseCookies(ctx);
+
+  const { data } = await apiClient.get("/dashboard");
+
+  const posts = data.map((post) => {
+    return {
+      id: post?.id,
+      title: post?.title,
+      rooms: post?.rooms,
+      price: post?.price,
+      square_meters: `${post?.square_meters}m²`,
+      created_at: format(parseISO(post?.created_at), "d MMM yyyy", {
+        locale: ptBR,
+      }),
+    };
+  });
 
   if (!token) {
     return {
@@ -29,5 +59,5 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     };
   }
 
-  return { props: {} };
+  return { props: { posts } };
 };
