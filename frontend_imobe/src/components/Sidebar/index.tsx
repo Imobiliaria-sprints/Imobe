b2dc1973bc023dcd41c87a96ad64728d8f391371
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-17 22:08:42 -0300

    Change sidebar, created button create post

--- frontend_imobe/src/components/Sidebar/index.tsx
+++ frontend_imobe/src/components/Sidebar/index.tsx
@@ -1,38 +1,46 @@
 import Image from "next/image";
 import { useModal } from "../../hooks/useModal";
-import { MdHome, MdFavorite, MdExitToApp } from "react-icons/md";
+import { MdHome, MdFavorite, MdExitToApp, MdAdd } from "react-icons/md";
 import styles from "./style.module.scss";
 export function Sidebar() {
   const { isActive, handleModalOpen } = useModal();
 
   return (
     <aside className={styles.sidebarContainer}>
-      <div onClick={handleModalOpen}>
-        {isActive ? (
-          <Image width={170} height={50} src="/icons/logo.svg" alt="logo" />
-        ) : (
-          <Image
-            width={50}
-            height={50}
-            src="/icons/miniLogo.svg"
-            alt="Mini logo"
-          />
-        )}
+      <div>
+        <div onClick={handleModalOpen}>
+          {isActive ? (
+            <Image width={170} height={50} src="/icons/logo.svg" alt="logo" />
+          ) : (
+            <Image
+              width={50}
+              height={50}
+              src="/icons/miniLogo.svg"
+              alt="Mini logo"
+            />
+          )}
+        </div>
+        <nav className={styles.menu}>
+          <div>
+            <MdHome color="#3B4A5B" style={{ width: 30, height: 30 }} />
+            {isActive ? <span>Início</span> : ""}
+          </div>
+          <div>
+            <MdFavorite color="#3B4A5B" style={{ width: 30, height: 30 }} />
+            {isActive ? <span>Favoritos</span> : ""}
+          </div>
+          <div>
+            <MdExitToApp color="#ED394F" style={{ width: 30, height: 30 }} />
+            {isActive ? <span>Sair</span> : ""}
+          </div>
+        </nav>
       </div>
-      <nav className={styles.menu}>
-        <div>
-          <MdHome color="#3B4A5B" style={{ width: 30, height: 30 }} />
-          {isActive ? <span>Início</span> : ""}
-        </div>
-        <div>
-          <MdFavorite color="#3B4A5B" style={{ width: 30, height: 30 }} />
-          {isActive ? <span>Favoritos</span> : ""}
-        </div>
-        <div>
-          <MdExitToApp color="#ED394F" style={{ width: 30, height: 30 }} />
-          {isActive ? <span>Sair</span> : ""}
-        </div>
-      </nav>
+      <section>
+        <button>
+          <MdAdd color="#ffff" style={{ width: 30, height: 30 }} />{" "}
+          {isActive ? <span>Criar anúncio</span> : ""}
+        </button>
+      </section>
     </aside>
   );
 }
