b1ab8301f428bac71851e4da19238022819cdd45
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-19 20:52:02 -0300

    Removed query for created uuid

--- backend_imobe/src/database/migration/1624136766532-CreateUsers.ts
+++ backend_imobe/src/database/migration/1624136766532-CreateUsers.ts
@@ -2,7 +2,6 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUsers1624136766532 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: "users",
@@ -11,7 +10,6 @@ export class CreateUsers1624136766532 implements MigrationInterface {
             name: "id",
             type: "uuid",
             isPrimary: true,
-            default: "uuid_generate_v4()",
           },
           {
             name: "name",
@@ -47,6 +45,5 @@ export class CreateUsers1624136766532 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropTable("users");
-    await queryRunner.query('DROP EXTENSION "uuid-ossp"');
   }
 }
