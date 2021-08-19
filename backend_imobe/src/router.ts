8d6ceb32e9d4a422eb2f5aa35b708eae412484e9
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-19 20:55:15 -0300

    Remove imports in routes file

--- backend_imobe/src/router.ts
+++ backend_imobe/src/router.ts
@@ -1,15 +1,6 @@
 import { Router } from "express";
-import CreateUserController from "./useCases/CreateUserUseCase/CreateUserController";
-import CreateUserAuthenticateController from "./useCases/CreateUserAuthenticated/CreateUserAuthenticateController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
-import CreateAdsController from "./useCases/CreateAdsUseCase/CreateAdsController";
 import ListAdsController from "./useCases/ListAdsUserUseCase/ListAdsController";
-import { validationUser } from "./useCases/CreateUserUseCase/validationUser";
-import { validationAds } from "./useCases/CreateAdsUseCase/validationAds";
-import CreateAddressController from "./useCases/CreateAddressUseCase/CreateAddressController";
-import { validationAddress } from "./useCases/CreateAddressUseCase/validationAddress";
-import ReturnUserAuthenticatedController from "./useCases/ReturnUserAuthenticatedUseCase/ReturnUserAuthenticatedController";
-import RefreshTokenController from "./useCases/RefreshTokenUserUseCase/RefreshTokenController";
 import { userRoutes } from "./routes/userRoutes";
 import { postRouter } from "./routes/postRoutes";
 
