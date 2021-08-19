f5ec8b3ffaa8b592b80b566a0a846cc60dd9486a
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-18 21:06:58 -0300

    Created refreshToken provider

---
+++ backend_imobe/src/provider/GenerateRefreshToken.ts
@@ -0,0 +1,18 @@
+import dayjs from "dayjs";
+import { getCustomRepository } from "typeorm";
+import { RefreshTokenRepository } from "../repositories/factory/RefreshTokenRepository";
+
+class GenerateRefreshToken {
+  async execute(user_id: string) {
+    const refreshToken = getCustomRepository(RefreshTokenRepository);
+    const expires_in = dayjs().add(1, "day").unix();
+
+    const generateRefreshToken = refreshToken.create({ user_id, expires_in });
+
+    await refreshToken.save(generateRefreshToken);
+
+    return generateRefreshToken;
+  }
+}
+
+export { GenerateRefreshToken };
