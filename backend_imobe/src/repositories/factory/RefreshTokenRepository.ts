636e837e5c10906076c72d91bdcd9ed984f23a29
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-18 21:07:43 -0300

    Created refreshToken repository

---
+++ backend_imobe/src/repositories/factory/RefreshTokenRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { RefreshToken } from "../../entities/RefreshToken";
+
+@EntityRepository(RefreshToken)
+class RefreshTokenRepository extends Repository<RefreshToken> {}
+
+export { RefreshTokenRepository };
