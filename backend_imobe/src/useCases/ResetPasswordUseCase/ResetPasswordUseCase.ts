aac0bb234fb3b5dd9a65bcab606c56de68cae119
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-24 21:30:18 -0300

    Created reset password use case and controller

---
+++ backend_imobe/src/useCases/ResetPasswordUseCase/ResetPasswordUseCase.ts
@@ -0,0 +1,21 @@
+import { IUserRepository } from "../../repositories/IUserRepository";
+
+class ResetPasswordUseCase {
+  constructor(private userRepository: IUserRepository) {}
+
+  async execute(email: string, token: string, password: string) {
+    const user = await this.userRepository.findOneUserByEmail(email);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (token !== user.password_reset_token) {
+      throw new Error("Token invalid");
+    }
+
+    await this.userRepository.updatePassword(user.id, password);
+  }
+}
+
+export { ResetPasswordUseCase };
