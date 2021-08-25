495c404ca697742aa88820a98156f89620da85e1
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-24 21:30:04 -0300

    Created forgot password use case and controller

---
+++ backend_imobe/src/useCases/ForgotPasswordUseCase/ForgotPasswordController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SendMailProvider } from "../../provider/SendMailProvider";
+import { UserRepository } from "../../repositories/factory/UserRepository";
+import { ForgotPasswordUseCase } from "./ForgotPasswordUseCase";
+
+class ForgotPasswordController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const userRepository = getCustomRepository(UserRepository);
+    const sendMailProvider = new SendMailProvider();
+
+    const forgotPasswordUseCase = new ForgotPasswordUseCase(
+      sendMailProvider,
+      userRepository
+    );
+
+    await forgotPasswordUseCase.execute(email);
+
+    return response.json();
+  }
+}
+
+export default new ForgotPasswordController();
---
+++ backend_imobe/src/useCases/ForgotPasswordUseCase/ForgotPasswordUseCase.ts
@@ -0,0 +1,50 @@
+import path from "path";
+import { ISendMailProvider } from "../../interfaces/ISendMailProvider";
+import { GenerateTokenProvider } from "../../provider/GenerateTokenProvider";
+import { IUserRepository } from "../../repositories/IUserRepository";
+
+class ForgotPasswordUseCase {
+  /**
+   *
+   * @param sendMailProvide
+   * @param userRepository
+   */
+  constructor(
+    private sendMailProvide: ISendMailProvider,
+    private userRepository: IUserRepository
+  ) {}
+
+  async execute(email: string) {
+    const user = await this.userRepository.findOneUserByEmail(email);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const generateTokenProvider = new GenerateTokenProvider();
+
+    const token = await generateTokenProvider.execute(user.id);
+
+    await this.userRepository.updatedPasswordToken(user.id, token);
+
+    const forgotPasswordMail = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "views",
+      "emails",
+      "forgotPasswordMail.hbs"
+    );
+
+    await this.sendMailProvide.execute({
+      to: email,
+      subject: "Esqueceu sua senha",
+      variables: {
+        token,
+      },
+      path: forgotPasswordMail,
+    });
+  }
+}
+
+export { ForgotPasswordUseCase };
