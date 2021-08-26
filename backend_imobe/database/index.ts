7b612d319cae155f20e6cd978385ea88396adc22
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-08-26 00:09:37 -0300

    Moved database to src

--- backend_imobe/database/index.ts
+++
@@ -1,8 +0,0 @@
-import { createConnection, Connection } from "typeorm";
-
-export default async (): Promise<Connection> => {
-  console.log(
-    `Create connection with database in mode ${process.env.NODE_ENV}`
-  );
-  return createConnection();
-};
