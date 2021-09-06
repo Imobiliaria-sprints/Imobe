963d28a5467e6fb1661c6e6cc6b1a366446c7d41
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-09-06 14:44:59 -0300

    Starting the creation announcement page

---
+++ frontend_imobe/src/pages/announcements.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Header } from "../components/Header";
+
+export default function Announcements() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
