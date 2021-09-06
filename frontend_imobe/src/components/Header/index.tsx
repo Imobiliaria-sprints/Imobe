c2346d31a250b2c14d0105c919cf8203ee4e3eca
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-09-06 14:44:16 -0300

    Change header and adding dropdown with mouseEnter event

--- frontend_imobe/src/components/Header/index.tsx
+++ frontend_imobe/src/components/Header/index.tsx
@@ -1,17 +1,47 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import styles from "./style.module.scss";
+import { MdExpandMore as ArrowDown, MdHome } from "react-icons/md";
+import { FaUserAlt as UserIcon } from "react-icons/fa";
+import Dropdown from "../Dropdown";
+import { useRouter } from "next/router";
 
-type HeaderProps = {
-  children: ReactNode;
-};
+export function Header() {
+  const [dropdownIsActive, setDropdownIsActive] = useState(false);
 
-export function Header({ children }: HeaderProps) {
+  const router = useRouter();
+
   return (
-    <div className={styles.headerContainer}>
-      <img src="/icons/logo.svg" alt="Logomarca" />
-      <nav>{children}</nav>
+    <div
+      className={styles.headerContainer}
+      onMouseLeave={() => setDropdownIsActive(false)}
+    >
+      <div onClick={() => router.push("/")}>
+        <img src="/icons/logo.svg" alt="Logomarca" draggable="false" />
+      </div>
+      <nav
+        className={styles.navContainer}
+        onMouseEnter={() => setDropdownIsActive(true)}
+      >
+        <div className={styles.userInfo}>
+          <section>
+            <UserIcon size="18" color="#294461" />
+            <span>Conta</span>
+          </section>
+          <ArrowDown size="18" color="#294461" />
+        </div>
+        {dropdownIsActive && (
+          <Dropdown>
+            <button onClick={() => router.push("/sign-in")}>Entrar</button>
+            <div>
+              <MdHome size="25" color="#294461" />
+              <span>Ver meu anúncios</span>
+            </div>
+            <div>Acessar a minha conta</div>
+            <div>Acessar a minha conta</div>
+          </Dropdown>
+        )}
+      </nav>
     </div>
   );
 }
