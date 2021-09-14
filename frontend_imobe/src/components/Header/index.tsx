9c9abeb1432b39f9e8a0146afc38f018684d1c99
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-09-13 22:10:50 -0300

    Change style in header, dropdown and pagination components

--- frontend_imobe/src/components/Header/index.tsx
+++ frontend_imobe/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { ReactNode, useState } from "react";
 import styles from "./style.module.scss";
-import { MdExpandMore as ArrowDown, MdHome } from "react-icons/md";
+import { MdExpandMore as ArrowDown, MdFavorite, MdHome } from "react-icons/md";
 import { FaUserAlt as UserIcon } from "react-icons/fa";
 import Dropdown from "../Dropdown";
 import { useRouter } from "next/router";
@@ -12,23 +12,21 @@ export function Header() {
   const router = useRouter();
 
   return (
-    <div
-      className={styles.headerContainer}
-      onMouseLeave={() => setDropdownIsActive(false)}
-    >
+    <div className={styles.headerContainer}>
       <div onClick={() => router.push("/")}>
         <img src="/icons/logo.svg" alt="Logomarca" draggable="false" />
       </div>
       <nav
         className={styles.navContainer}
         onMouseEnter={() => setDropdownIsActive(true)}
+        onMouseLeave={() => setDropdownIsActive(false)}
       >
         <div className={styles.userInfo}>
           <section>
-            <UserIcon size="18" color="#294461" />
+            <UserIcon size="15" color="#474747" />
             <span>Conta</span>
           </section>
-          <ArrowDown size="18" color="#294461" />
+          <ArrowDown size="18" color="#474747" />
         </div>
         {dropdownIsActive && (
           <Dropdown>
@@ -37,8 +35,10 @@ export function Header() {
               <MdHome size="25" color="#294461" />
               <span>Ver meu anúncios</span>
             </div>
-            <div>Acessar a minha conta</div>
-            <div>Acessar a minha conta</div>
+            <div>
+              <MdFavorite size="25" color="#294461" />
+              <span>Minhas curtidas</span>
+            </div>
           </Dropdown>
         )}
       </nav>
