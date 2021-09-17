f67e156bcfbc546f175982e658bb69a87fa81738
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-09-16 22:05:08 -0300

    refactor(style): Changeg structure and style

--- frontend_imobe/src/pages/announcements.tsx
+++ frontend_imobe/src/pages/announcements.tsx
@@ -5,9 +5,13 @@ import { Header } from "../components/Header";
 import { Pagination } from "../components/Pagination";
 import { useAnnouncement } from "../hooks/useAnnouncement";
 import styles from "../styles/pages/announcements.module.scss";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { useSlide } from "../hooks/useSlide";
 
 export default function Announcements(props) {
   const [page, setPage] = useState(1);
+
+  const { lastImage, nextImage, slideImages } = useSlide();
 
   const { data, isLoading, isFetching, error } = useAnnouncement(page);
   console.log(data);
@@ -30,31 +34,37 @@ export default function Announcements(props) {
               <div className={styles.searchResults}>
                 <h1>{data?.totalCount} imóveis para você escolher</h1>
               </div>
-              {data.announcements.map((announcement) => {
+              {data.announcements.map((announcement, index) => {
                 return (
-                  <div key={announcement.id} className={styles.announcement}>
+                  <div key={announcement?.id} className={styles.announcement}>
                     <header>
                       <div>
                         <img
-                          src={announcement.user.avatar}
-                          alt={announcement.user.name}
+                          src={announcement?.user.avatar}
+                          alt={announcement?.user.name}
                         />
-                        <span>{announcement.user.name}</span>
+                        <span>{announcement?.user.name}</span>
                       </div>
-                      <span>{announcement.created_at}</span>
+                      <span>{announcement?.created_at}</span>
                     </header>
-                    <figure>
+                    <figure className={styles.slide_image}>
+                      <div onClick={lastImage}>
+                        <MdKeyboardArrowLeft color="#fff" />
+                      </div>
                       <img
-                        src={announcement.images[0].url}
-                        alt={announcement.title}
+                        src={slideImages(announcement?.images).url}
+                        alt={announcement?.title}
                       />
+                      <div onClick={nextImage}>
+                        <MdKeyboardArrowRight color="#fff" />
+                      </div>
                     </figure>
-                    <span>{announcement.title}</span>
+                    <span>{announcement?.title}</span>
                     <ul>
-                      <li>{announcement.rooms}</li>
-                      <li>{announcement.square_meters}</li>
+                      <li>{announcement?.rooms}</li>
+                      <li>{announcement?.square_meters}</li>
                     </ul>
-                    <h2>{announcement.price}</h2>
+                    <h2>{announcement?.price}</h2>
                   </div>
                 );
               })}
