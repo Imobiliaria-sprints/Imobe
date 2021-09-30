cc9cdb75ab6266f8e5714094bcb2d47ed7e051d1
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-09-30 20:18:26 -0300

    created(useFetch) Adding useSWR and created service useFetch

---
+++ frontend_imobe/src/services/useFetch.ts
@@ -0,0 +1,20 @@
+import useSWR from "swr";
+import { api } from "./api";
+
+export const useFetch = <Data = any, Error = any>(
+  url: string,
+  token?: string
+) => {
+  const { data, error, mutate, isValidating } = useSWR<Data, Error>(
+    url,
+    async (url) => {
+      const { data } = await api.get(url, {
+        headers: { Authorization: `Bearer  ${token}` },
+      });
+
+      return data;
+    }
+  );
+
+  return { data, error, mutate, isValidating };
+};
