8d6a26de20b6e00f51b100dc4491761785b37d8d
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-10-03 17:40:08 -0300

    refactor(useDrop): Created context and get in useDrop

--- frontend_imobe/src/hooks/useDrop.ts
+++ frontend_imobe/src/hooks/useDrop.ts
@@ -1,37 +1,6 @@
-import { useState } from "react";
-import { v4 as uuid } from "uuid";
+import { useContext } from "react";
+import { DropzoneContext } from "../context/DropzoneContext";
 
-type DropzoneData = {
-  files: Files[];
-  handleFiles: (files_accept: File[]) => void;
-  handleDeleteFile: (file_id: string) => void;
-};
-
-interface Files extends File {
-  id: string;
-  preview: string;
-}
-
-export const useDrop = (): DropzoneData => {
-  const [files, setFiles] = useState<Files[]>([]);
-
-  function handleFiles(files_accept: File[]) {
-    const uploadedFiles = files_accept.map((file) =>
-      Object.assign(file, {
-        id: uuid(),
-        preview: URL.createObjectURL(file),
-      })
-    );
-    if (files.length >= 5) {
-      return;
-    }
-    setFiles(files.concat(uploadedFiles));
-  }
-
-  function handleDeleteFile(file_id: string) {
-    setFiles(files.filter((file) => file.id !== file_id));
-    console.log(files);
-  }
-
-  return { files, handleFiles, handleDeleteFile };
+export const useDrop = () => {
+  return useContext(DropzoneContext);
 };
