43814364776f9f4749887ac9c5c8d31b4ae5ee6f
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-10-16 14:08:39 -0300

    refactor(routes): Push CreateAddresUseCase index file

--- backend_imobe/src/infra/http/routes/announcement.routes.ts
+++ backend_imobe/src/infra/http/routes/announcement.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "@middle/ensureAuthenticated";
-import { CreateAddressController } from "@modules/imobeAddress/useCases/CreateAddressUseCase/CreateAddressController";
 import { CreateAnnouncementController } from "@modules/imobeAnnouncement/useCases/CreateAnnouncementUseCase/CreateAnnouncementController";
 import { ListAllAnnouncementController } from "@modules/imobeAnnouncement/useCases/ListAllAnnouncementUseCase/ListAllAnnouncementController";
 import { SearchAnnouncementController } from "@modules/imobeAnnouncement/useCases/SearchAnnouncementUseCase/SearchAnnouncementController";
 import { DeleteAnnouncementController } from "@modules/imobeAnnouncement/useCases/DeleteAnnouncementUseCase/DeleteAnnouncementController";
+import {createAddressController} from "@modules/imobeAddress/useCases/CreateAddressUseCase";
 import uploadImageConfig from "@config/uploadImageConfig";
 import multer from "multer";
 
@@ -12,7 +12,6 @@ const upload = multer(uploadImageConfig);
 
 const announcement = Router();
 
-const createAddressController = new CreateAddressController();
 const createAnnouncementController = new CreateAnnouncementController();
 const listAllAnnouncementController = new ListAllAnnouncementController();
 const searchAnnouncementController = new SearchAnnouncementController();
