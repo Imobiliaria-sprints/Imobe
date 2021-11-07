3be54ea9b45626d48d24c07ce82d4643ff3e357e
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-11-07 18:54:29 -0300

    feat(style): alterando o estilo do rodapé da página

--- frontend_imobe/src/components/Footer/index.tsx
+++ frontend_imobe/src/components/Footer/index.tsx
@@ -7,37 +7,39 @@ import { MdSend } from "react-icons/md";
 export function Footer() {
   return (
     <div className={styles.footer_container}>
-      <div className={styles.contain_logo}>
-        <Image height="60" width="200" src="/icons/logo.svg" alt="logomark" />
-      </div>
-        <div className={styles.content_topic}>
-          <Topic name="Sobre">
-            <li>Lorem ipsum dolar</li>
-            <li>Lorem ipsum dolar</li>
-            <li>Lorem ipsum dolar</li>
-          </Topic>
-          <Topic name="Política">
-            <li>Política de privacidade</li>
-            <li>Sobre nós</li>
-          </Topic>
+      <footer>
+        <div className={styles.contain_logo}>
+          <Image height="60" width="200" src="/icons/logo.svg" alt="logomark" />
         </div>
-      <div className={styles.send_notification}>
-        <span>Quer receber as novidades da Imobe?</span>
-        <section>
-          <input placeholder="Digite seu melhor email" />
-          <button>
-            <MdSend color="#fff" size="20" />
-          </button>
-        </section>
-        <section>
-          <div>
-            <FaFacebookF color="#fff" size="20" />
+          <div className={styles.content_topic}>
+            <Topic name="Sobre o Projeto">
+              <li>Projeto Interdisciplinar</li>
+              <li>Cruzeiro do Sul</li>
+              <li>Imobiliária</li>
+            </Topic>
+            <Topic name="Política">
+              <li>Política de privacidade</li>
+              <li>Sobre nós</li>
+            </Topic>
           </div>
-          <div>
-            <FaInstagram color="#fff" size="20" />
-          </div>
-        </section>
-      </div>
+        <div className={styles.send_notification}>
+          <span>Quer receber as novidades da Imobe?</span>
+          <section>
+            <input placeholder="Digite seu melhor email" />
+            <button>
+              <MdSend color="#fff" size="20" />
+            </button>
+          </section>
+          <section>
+            <div>
+              <FaFacebookF color="#fff" size="20" />
+            </div>
+            <div>
+              <FaInstagram color="#fff" size="20" />
+            </div>
+          </section>
+        </div>
+      </footer>
     </div>
   );
 }
