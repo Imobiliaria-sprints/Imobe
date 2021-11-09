cecc9abfb65c888e06280c8c8082ef6df29bf1c9
Lucas-Duarte-dev <lucasduarteb2000@hotmail.com>
2021-11-09 19:09:15 -0300

    feat(announcement): removendo anúncios

--- frontend_imobe/src/pages/user/my-announcements.tsx
+++ frontend_imobe/src/pages/user/my-announcements.tsx
@@ -6,8 +6,14 @@ import {parseCookies} from "nookies";
 import {useFetch} from "../../hooks/useFetch";
 import styles from '../../styles/pages/user/my-announcements.module.scss';
 import {FaBed} from "react-icons/fa";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useRouter} from "next/router";
+import {IoMdTrash} from "react-icons/io";
+import {api} from "../../services/api";
+import {toast, Toaster} from "react-hot-toast";
+import Modal from "react-modal";
+import {modalCustomStyles} from "../../utils/ModalStyleConf";
+import {response} from "express";
 
 type Announcement = {
     id: string;
@@ -40,18 +46,47 @@ export default function  MyAnnouncement() {
 
     const router = useRouter();
 
+    const [announcements, setAnnouncements] = useState<Announcement[] | null>();
+    const [isActive, setIsActive] = useState(false);
 
     const { ["imobeflex.token"]: token } = parseCookies();
 
+    useEffect(() => {
+        api.get<Announcement[]>('announcement/user', {
+            headers: { Authorization: `Bearer ${token}` },
+        }).then(response => setAnnouncements(response.data));
+    }, [])
+
     const {data} = useFetch<Announcement[]>('announcement/user', token);
+
+    function handleModalOpen(): void {
+        return setIsActive(!isActive);
+    }
+
+    async function handleDeleteAnnouncement(id: string) {
+        const {status} = await api.delete(`announcement/${id}`, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+
+        if(status !== 200) {
+            return toast.error("Não foi possível remover o anúncio.");
+        }
+
+        setAnnouncements(announcements.filter(announce => announce.id !== id));
+
+        toast.success("Seu imóvel foi removido!");
+
+        handleModalOpen();
+    }
 
     return (
         <div className={styles.my_announcement_container}>
             <Sidebar />
+            <Toaster />
             <div className={styles.content_announcement_list}>
                 <h1>Meus anúncios</h1>
                 <section className={styles.content_list}>
-                    {data?.map(announce => {
+                    {announcements?.map(announce => {
                       return (
                           <div key={announce.id}>
                               <section>
@@ -63,15 +98,35 @@ export default function  MyAnnouncement() {
                               <div>
                                   <ul>
                                       {Number(announce.rooms) !== 0 && <li> <FaBed  size="20" />  {announce.rooms}</li>}
-                                      <li>{announce.square_meters}</li>
+                                      <li>{announce.square_meters} m²</li>
                                   </ul>
-                                  <button type="button" onClick={() => router.push(`/publish/${announce?.id}`)}>Ver publicação</button>
+                                  <div className={styles.actions}>
+                                    <button onClick={handleModalOpen}><IoMdTrash size="20" color="#ed394f"/></button>
+                                    <button type="button" onClick={() => router.push(`/publish/${announce?.id}`)}>Ver publicação</button>
+                                  </div>
                               </div>
+                              <Modal
+                                  isOpen={isActive}
+                                  style={modalCustomStyles}
+                                  onRequestClose={handleModalOpen}
+                                  contentLabel="Exit to app Modal"
+                              >
+                                  <div className={styles.exitToApp}>
+                                      <h2>Você tem certeza que deseja excluir esse anúncio?</h2>
+                                      <button type="button" onClick={handleModalOpen}>
+                                          Não
+                                      </button>
+                                      <button type="button" onClick={() =>handleDeleteAnnouncement(announce.id)}>
+                                          Sim
+                                      </button>
+                                  </div>
+                              </Modal>
                           </div>
                       );
                     })}
                 </section>
             </div>
+
         </div>
     )
 }
